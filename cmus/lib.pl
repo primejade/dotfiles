73fb159678fd20fcbdd69f4c58c876bc4b0db188
primejade <88491070+primejade@users.noreply.github.com>
2021-08-17 16:37:02 +0430

    Add files via upload

---
+++ cmus/lib.pl
@@ -0,0 +1,113 @@
+/home/hos/Music/Musics/Boom 13-31 01Feb.mp3
+/home/hos/Music/Musics/BoomFeb.mp3
+/home/hos/Music/Musics/C2zbBGQ6kK.mp3
+/home/hos/Music/Musics/ChandShanbasEmroozRingtone.mp3
+/home/hos/Music/Musics/Dirt 13-06 26Feb.mp3
+/home/hos/Music/Musics/Kick 02-12 28Feb.mp3
+/home/hos/Music/Musics/Night Car 17-50 12Mar.mp3
+/home/hos/Music/Musics/Night Car 17-59 12Mar.mp3
+/home/hos/Music/Musics/Pishro_Vs_Wilson_-_Ring_2_Remix_by_30Bam.mp3
+/home/hos/Music/Musics/Sad 01-22 28Feb.mp3
+/home/hos/Music/Musics/Sea 02-50 28Feb.mp3
+/home/hos/Music/Musics/Shadow 10-20 01Feb.mp3
+/home/hos/Music/Musics/Sin City 02-01 28Feb.mp3
+/home/hos/Music/Musics/Snap Back 13-58 02Feb.mp3
+/home/hos/Music/Musics/Snap Back 14-14 01Feb.mp3
+/home/hos/Music/Musics/Voltage 00-34 28Feb.mp3
+/home/hos/Music/Musics/last_song_I'll_ever_write_about_you_-_carly_butler.mp3
+/home/hos/Music/Musics/88381741.mp3
+/home/hos/Music/Musics/Ethnic Trap 29-12-2020 04-5.mp3
+/home/hos/Music/Musics/_Ehsan KhajehAmiri - vaghti Mikhandi (320).mp3
+/home/hos/Music/Musics/Zalooha Khoone Del Mikhoran.mp3
+/home/hos/Music/Musics/Ali Sorena - Gavazn/15.alisorena_16.6.mp3
+/home/hos/Music/Musics/Ali Sorena - Gavazn/16.alisorena_asayeshgah.mp3
+/home/hos/Music/Musics/Ali Sorena - Gavazn/07.alisorena_baad_pichid_too_mazraye_gandom.mp3
+/home/hos/Music/Musics/Ali Sorena - Gavazn/05.alisorena_birahe.mp3
+/home/hos/Music/Musics/Ali Sorena - Gavazn/18.alisorena_cut.mp3
+/home/hos/Music/Musics/Ali Sorena - Gavazn/03.alisorena_daar-dastane_maryam-.mp3
+/home/hos/Music/Musics/Ali Sorena - Gavazn/10.alisorena_daroon_-skit-.mp3
+/home/hos/Music/Musics/Ali Sorena - Gavazn/17.alisorena_dashte_parvaneha.mp3
+/home/hos/Music/Musics/Ali Sorena - Gavazn/11.alisorena_gavazn.mp3
+/home/hos/Music/Musics/Ali Sorena - Gavazn/08.alisorena_ghalbhaye_atashin.mp3
+/home/hos/Music/Musics/Ali Sorena - Gavazn/04.alisorena_goriz_az_markaz.mp3
+/home/hos/Music/Musics/Ali Sorena - Gavazn/01.alisorena_harkat_-arrabeye_marg-.mp3
+/home/hos/Music/Musics/Ali Sorena - Gavazn/12.alisorena_istgahe_shahre_yakh.mp3
+/home/hos/Music/Musics/Ali Sorena - Gavazn/09.alisorena_laghzesh.mp3
+/home/hos/Music/Musics/Ali Sorena - Gavazn/14.alisorena_na_mimiram_na_barmigardam.mp3
+/home/hos/Music/Musics/Ali Sorena - Gavazn/02.alisorena_nakhoda_jelal.mp3
+/home/hos/Music/Musics/Ali Sorena - Gavazn/13.alisorena_pelikan.mp3
+/home/hos/Music/Musics/Ali Sorena - Gavazn/06.alisorena_raghs_-pichak-.mp3
+/home/hos/Music/Musics/Sadegh Ft. Pedram Azad - Nahang To Hoz Hamoon Mahi Ghermeze.mp3
+/home/hos/Music/Musics/Bad Boys   001_Bob Marley.mp3
+/home/hos/Music/Musics/2Pac-California-Love-192.mp3
+/home/hos/Music/Musics/Ali Sorena Haroomi Bezan.mp3
+/home/hos/Music/Musics/Ali-Sorena-Nakhoda-Jelal-320.mp3
+/home/hos/Music/Musics/18-Cut [Prod. Ali Sorena]@Rapnews_Times.mp3
+/home/hos/Music/Musics/Ali Sorena - Dard Haye Vagheei.mp3
+/home/hos/Music/Musics/Ali-Yasini-Ye-Saniye-320.mp3
+/home/hos/Music/Musics/Amir-Khalvat-Super-Star-320.mp3
+/home/hos/Music/Musics/Amir-Tataloo-Mikham-320.mp3
+/home/hos/Music/Musics/Amir-Tataloo-Ye-Saram-Be-Ma-Bezan-320.mp3
+/home/hos/Music/Musics/eshtabahKhoob/01-Khoob.m4a
+/home/hos/Music/Musics/eshtabahKhoob/02-Saz.m4a
+/home/hos/Music/Musics/eshtabahKhoob/03-Lams.m4a
+/home/hos/Music/Musics/eshtabahKhoob/04-Naghsh.m4a
+/home/hos/Music/Musics/eshtabahKhoob/05-Negah.m4a
+/home/hos/Music/Musics/eshtabahKhoob/06-Jang.m4a
+/home/hos/Music/Musics/eshtabahKhoob/07-Tekrar.m4a
+/home/hos/Music/Musics/eshtabahKhoob/08-Niaz.m4a
+/home/hos/Music/Musics/eshtabahKhoob/09-Zakhm.m4a
+/home/hos/Music/Musics/eshtabahKhoob/10-Rishe.m4a
+/home/hos/Music/Musics/eshtabahKhoob/11-Momken.m4a
+/home/hos/Music/Musics/eshtabahKhoob/12-Mordab.m4a
+/home/hos/Music/Musics/eshtabahKhoob/13-Sooz.m4a
+/home/hos/Music/Musics/eshtabahKhoob/14-Boresh.m4a
+/home/hos/Music/Musics/eshtabahKhoob/15-Solh.m4a
+/home/hos/Music/Musics/eshtabahKhoob/16-Eshtebah.m4a
+/home/hos/Music/Musics/Bahram-Adam-320.mp3
+/home/hos/Music/Musics/Bahram - Beshno Prod. by Peymandegar.mp3
+/home/hos/Music/Musics/bahram_feat_pishro_afsoos_-_remixed_by_saeed_payab.mp3
+/home/hos/Music/Musics/BeatRapOfficial - Hiphop Beat (249).mp3
+/home/hos/Music/Musics/BeatRapOfficial - Matinmv Hiphop Beat.mp3
+/home/hos/Music/Musics/BeatRapOfficial - Oldschool Beat (147).mp3
+/home/hos/Music/Musics/BeatRapOfficial - Trap Beat (303).mp3
+/home/hos/Music/Musics/SEYEDNO1.mp3
+/home/hos/Music/Musics/Daniyal - Khaste Shodam (320).mp3
+/home/hos/Music/Musics/Fadaei-Naji-256.mp3
+/home/hos/Music/Musics/Fadaei-Jenayi-128.mp3
+/home/hos/Music/Musics/Fadaei-Mostaghel-320.mp3
+/home/hos/Music/Musics/Fadaei-Efrati-320.mp3
+/home/hos/Music/Musics/Fadaei-Abi-Ghermez-320.mp3
+/home/hos/Music/Musics/Fadaei-Dige-Tablo-Shode-320.mp3
+/home/hos/Music/Musics/Fadaei - Koli.mp3
+/home/hos/Music/Musics/Hichkas-Firooz-256.mp3
+/home/hos/Music/Musics/Homayoun Shajarian - Abr Mibarad (320).mp3
+/home/hos/Music/Musics/14-14 01FebChecked.mp3
+/home/hos/Music/Musics/Dirt 13-06 26Feb (2).mp3
+/home/hos/Music/Musics/James-Brown-I-Got-You-(I-Feel-Good)-192.mp3
+/home/hos/Music/Musics/Kaaj - Zaviye did (2).mp3
+/home/hos/Music/Musics/Kaaj - Zaviye did.mp3
+/home/hos/Music/Musics/code v2.mp3
+/home/hos/Music/Musics/Matin Mv - Over Instrumental.mp3
+/home/hos/Music/Musics/Mohammad-Esfahani-Maahiha-320.mp3
+/home/hos/Music/Musics/Mohammad-Esfahani-Havamo-Nadashti-320.mp3
+/home/hos/Music/Musics/Snap Back 14-14 01FebCc (2).mp3
+/home/hos/Music/Musics/Snap Back 14-14 01FebCc.mp3
+/home/hos/Music/Musics/Parvaz Homay - Khake Nefrin Shode (320).mp3
+/home/hos/Music/Musics/Purya-Putak-Rape-Ma-(Ft-Sunboy)-320.mp3
+/home/hos/Music/Musics/Rez Nabood.mp3
+/home/hos/Music/Musics/Reza-Pishro-Cypher-(Ft-Ali-Owj-Ava-Ghadar)-320.mp3
+/home/hos/Music/Musics/Reza-Pishro-Shock-320.mp3
+/home/hos/Music/Musics/Reza-Pishro-Rap-Game-(Ft-Ali-Owj)-320.mp3
+/home/hos/Music/Musics/Sadegh-Bi-Seda-Bi-Soorat-320.mp3
+/home/hos/Music/Musics/Sadegh-Nahang-To-Hoz-Hamoon-Mahi-Ghermeze-(Ft-Pedram-Azad)-3.mp3
+/home/hos/Music/Musics/Sadegh-Poolo-Ghodrat-320.mp3
+/home/hos/Music/Musics/Sadegh-To-Hayat-Nabashe-Vahsh-320.mp3
+/home/hos/Music/Musics/Sadegh-Ye-Serri-To-Ye-Seri-320 (2).mp3
+/home/hos/Music/Musics/Sadegh-Ye-Serri-To-Ye-Seri-320.mp3
+/home/hos/Music/Musics/Sadegh-Zalooha-Khoone-Del-Mikhoran-320.mp3
+/home/hos/Music/Musics/Shahin-Najafi-Parvaz-(Ft-Babak-Amini)-320.mp3
+/home/hos/Music/Musics/Tik-Taak-Rooz-320.mp3
+/home/hos/Music/Musics/Yaser-Binam-Salam-Reies-320.mp3
+/home/hos/Music/Musics/Yaser-Binam-Haj-Agha-320.mp3
+/home/hos/Music/Musics/4_384781318275204157.mp3
